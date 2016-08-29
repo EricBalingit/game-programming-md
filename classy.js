1a10378d818cd1c0f922e440ad17fcd63fb5ac54
ericbalingit <embalingit.github@gmail.com>
2016-08-29 20:11:54 +0000

    adding anchor support

--- classy.js
+++ classy.js
@@ -2,6 +2,7 @@
 
 var classy,
   renderersToReplace = [
+    { pattern: "a", fullName: "anchor" },
     { pattern: "p", fullName: "paragraph" },
     { pattern: "h\\d", fullName: "heading" },
     { pattern: "ul", fullName: "bullet_list" },
