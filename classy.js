8594e4e8c7c6af75334013ae2d8fe93d1608fe7e
ericbalingit <embalingit.github@gmail.com>
2016-08-29 20:53:18 +0000

    update classy anchor support

--- classy.js
+++ classy.js
@@ -2,7 +2,7 @@
 
 var classy,
   renderersToReplace = [
-    { pattern: "a", fullName: "anchor", inline: true },
+    { pattern: "a", fullName: "anchor" },
     { pattern: "p", fullName: "paragraph" },
     { pattern: "h\\d", fullName: "heading" },
     { pattern: "ul", fullName: "bullet_list" },
